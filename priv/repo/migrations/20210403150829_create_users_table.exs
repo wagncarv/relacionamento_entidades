c02ff28f3e7aba3a020c836ce3b94812f9a76eb3
Wagner Carvalho <wagncarv@gmail.com>
2021-04-03 12:15:02 -0300

    migration user

---
+++ priv/repo/migrations/20210403150829_create_users_table.exs
@@ -0,0 +1,15 @@
+defmodule RelacionamentoEntidades.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :cpf, :string
+      add :email, string
+
+      timestamps()
+    end
+  end
+  create unique_index(:users, [:cpf])
+  create unique_index(:users, [:email])
+end
